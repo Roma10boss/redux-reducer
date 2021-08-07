f55e2a15e0df094bdd1200c9bc23b520ecf52f5e
coolc <Romagustave10@gmail.com>
2021-08-07 12:54:27 -0400

    done

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
-export function managePresents(state, action){
+let state = {numberOfPresents: 1}
+
+let action = {
+  type: "presents/increase",
+};
+
+export function managePresents(state, action) {
+  switch (action.type) {
+    case "presents/increase":
+      return {numberOfPresents: state.numberOfPresents+1}
+      default:
+        return state
+  }
+
 }
